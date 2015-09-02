ebb6c5f9036715e7b60be0f7f900276c74c04727
Daniels Pitkevics <daniels.pitkevics@gmail.com>
2015-09-02 08:08:05 -0400

    Added active colors for navbar links

--- DevNet/settings.py
+++ DevNet/settings.py
@@ -121,7 +121,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'semantic'),
 )
 
 # Accounts Configuration
--- main/templatetags/sitetags.py
+++ main/templatetags/sitetags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.urlresolvers import reverse
 
 from DevNet import local_settings
 
@@ -9,3 +10,10 @@ register = template.Library()
 @register.simple_tag
 def get_socket_url():
     return local_settings.SOCKET_URL
+ 
+ 
+@register.simple_tag
+def current(request, urls):
+    if request.path in ( reverse(url) for url in urls.split() ):
+        return "active-menu-item"
+    return ""
