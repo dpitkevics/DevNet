c56994024ed55295f7c8840dce7b08fc76f6b1b8
Daniels Pitkevics <daniels.pitkevics@gmail.com>
2015-09-03 05:02:33 -0400

    Server fixes

--- DevNet/gunicorn_config.py
+++ DevNet/gunicorn_config.py
@@ -1,8 +1,8 @@
-bind = '127.0.0.1:8008'
+bind = '127.0.0.1:8000'
 backlog = 2048
 workers = 1
 timeout = 30
-daemon = False
+daemon = True
 pidfile = 'gunicorn_pid'
-user = 'www-data'
-group = 'www-data'
+user = 'daniels'
+group = 'daniels'
